3882c6702292dfefef3514dc963ce05f06c6905d
unknown <Admin@CSLAB235.CSSERVER.local>
2024-02-01 19:57:59 +0800

    Activity 1

--- ConsoleAhh/ConsoleAhh/Program.cs
+++ ConsoleAhh/ConsoleAhh/Program.cs
@@ -1,2 +1,102 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace borgar
+{
+
+    public class Meth
+    {
+
+        public int plus(int n1, int n2)
+        {
+            return  n1 + n2;
+
+        }
+        public int negartiv(int n1, int n2)
+        {
+            return n1-n2;
+        }
+        public int multi(int n1, int n2)
+        {
+            return n1*n2;
+        }
+        public int divi(int n1, int n2)
+        {
+            return n1 / n2;
+
+            /*public void plus(int n1, int n2)
+            {
+                Console.WriteLine("Addition = " +(n1+n2));
+
+            }
+            public void negartiv(int n1, int n2)
+            {
+                Console.WriteLine("Subtraction = " + (n1-n2));
+
+            }
+            public void multi(int n1, int n2)
+            {
+                Console.WriteLine("Multi = " +  (n1*n2));
+
+            }
+            public void divi(int n1, int n2)
+            {
+                Console.WriteLine("Divi = " + (n1/n2));
+    */
+        }
+        /* public void Final {
+
+         }*/
+
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Meth meth = new Meth();
+
+            int n1, n2;
+
+            /*  Console.WriteLine("enter #1: ");
+              n1 = int.Parse(Console.ReadLine());
+              Console.WriteLine("enter #2: ");
+              n2 = int.Parse(Console.ReadLine());
+
+             meth.plus(n1, n2);
+             meth.negartiv(n1, n2);
+              meth.multi(n1, n2);
+              meth.divi(n1, n2);  */
+
+            Console.WriteLine("enter #1: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter #2: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Total (Addition) : " + meth.plus(n1, n2));
+
+            Console.WriteLine("enter #1: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter #2: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Total (Subtract) : " + meth.negartiv(n1, n2));
+
+            Console.WriteLine("enter #1: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter #2: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Total (Multi) : " + meth.multi(n1, n2));
+
+            Console.WriteLine("enter #1: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter #2: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Total (Division): " + meth.divi(n1,n2));
+
+        }
+    }
+}
--- ConsoleAhh/ConsoleAhh/obj/Release/net8.0/ConsoleAhh.AssemblyInfo.cs
+++ ConsoleAhh/ConsoleAhh/obj/Release/net8.0/ConsoleAhh.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleAhh")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9674884554fc48c78ec11fbb1bfc3e0379308e7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleAhh")]
